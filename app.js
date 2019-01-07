d60c8d11615c2f4857d751c26f6cf97117d8b9ae
jhatts <jhatts@hotmail.co.uk>
2019-01-07 23:03:52 +0000

    created filter tasks function

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // clear task event
   clearBtn.addEventListener('click', clearTasks);
+  // filter task event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // add task function
@@ -61,4 +63,17 @@ function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+// filter tasks function
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
