ef80fb9f3f78fa2cbac90f5feeef16331ae1a80c
jhatts <jhatts@hotmail.co.uk>
2019-01-07 22:36:41 +0000

    create remove task and clear task functions

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ loadEventListeners();
 function loadEventListeners() {
   // add task list
   form.addEventListener('submit', addTask);
+  // remove task event
+  taskList.addEventListener('click', removeTask);
+  // clear task event
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 // add task function
@@ -41,4 +45,20 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// remove task function
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// clear tasks function
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
